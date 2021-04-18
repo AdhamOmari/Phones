2373e4f0e88b3abb1b0ecb96cbacab9b5d7d2d73
AdhamOmari <>
2021-04-18 11:53:13 +0000

    creat fun

--- phones.js
+++ phones.js
@@ -1,5 +1,6 @@
 alert ("Welcom to Dr.phone")  
 
+
 var username = prompt ('enter your name') ;
 while ( username !== 'ali' && username !=='ahmed')
 {
@@ -8,12 +9,12 @@ while ( username !== 'ali' && username !=='ahmed')
 
  if(username == "ali"){
 
-   console.log('hello = ' + 'User ali ');
+   console.log('Hello = ' + 'User ali ');
  }
 
  else if (username == "ahmad"){
 
-   console.log ('hello = ' + 'user ahmad');
+   console.log ('Hello = ' + 'user ahmad');
  }
  
  var userage = prompt ('enter your age ?');
@@ -32,9 +33,23 @@ else if (userage >= 100){
 
 
 
+function askuser(planguage){
+  
+  var phonetype =prompt('what type phone do you like '+ planguage + '?');
+  return phonetype ;
+}
+
+ var answer1 = askuser('LG');
+console.log (answer1);
+
+askuser('Sony');
+console.log (answer1);
+   
+askuser('Lenovo'); 
+console.log (answer1);
+
 var numberimg  =prompt ('please inter how many phons do you want to order ?');
 for (var i =1 ; i<= numberimg ;i++ ){
-
 
  if (phonetype == 'samsung'){
    var imge = '<img src ="https://images.samsung.com/is/image/samsung/assets/levant/galaxy-s21/home/HOME_T_O_KV_IM-KV_624X352_MO.jpg?$624_352_PNG$"/> alt= "samsung"';
@@ -51,5 +66,8 @@ else if (phonetype == 'iphone'){
 
 var phone  ='<img src="img/04.jpg" alt = "iphone"/>';
 document.write(phone);
+
+
 }
+
 }
